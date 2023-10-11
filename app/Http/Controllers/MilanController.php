d86a565034088fbda8533e0db0210146addbc6fb
Mee-lonThapa <tmilan1010@gmail.com>
2023-10-11 09:29:13 +0545

    jitu sisako

---
+++ app/Http/Controllers/MilanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Milan;
+use Illuminate\Http\Request;
+
+class MilanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Milan $milan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Milan $milan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Milan $milan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Milan $milan)
+    {
+        //
+    }
+}
---
+++ resources/views/pratice.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  
+</head>
+<style>
+    form{
+        text-align: center;
+    }
+</style>
+<body>
+    <form action="">
+    <h1>Registrations <span>Form</span></h1>
+        <label for="">Name</label><br>
+        <input type="text"><br>
+
+        <label for="">Contact</label><br>
+        <input type="number"><br>
+
+        <label for="">Password</label><br>
+        <input type="password"><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
